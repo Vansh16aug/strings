79965a9f1d264bf032f821ef611db7c7fbf1505d
Vansh kumar <kumarvansh16aug@gmail.com>
2023-06-29 21:09:45 +0530

    Add files via upload

---
+++ strings.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cout<<"Enter a string: ";
+    cin>>s;
+    int len=s.length();
+    cout<<"Last letter :"<<s[len-1]<<endl;
+    return 0;
+}
